0b58b0b3bef849088e6c57a78d3117309474eb6b
joaop21 <jpsilva9898@gmail.com>
2019-02-24 22:44:06 +0000

    generator

---
+++ Fase1/Generator/generator.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("Parâmetros inválidos!!!\n");
+		return 0;
+	}
+
+	string primitive = argv[1];
+
+	if(primitive == "plane"){
+		// fazer cenas
+	} else if(primitive == "box"){
+		// fazer cenas
+	} else if(primitive == "sphere"){
+		// fazer cenas
+	} else if(primitive == "cone"){
+		// fazer cenas
+	} else{
+		printf("Parâmetros inválidos!!!\n");
+		return 0;
+	}
+
+}
