e3870d1e0d0c530ae757f5f5b8d814c7c6ad7ad9
joaop21 <jpsilva9898@gmail.com>
2019-03-04 14:27:59 +0000

    cmake para o generator

--- Fase1/Generator/sphere.h
+++ Fase1/Generator/sphere.h
@@ -11,18 +11,9 @@ int generateSphere(float radius, int slices, int stacks, string file_name){
 	float sliceStep = 2*M_PI / slices;
 	float stackStep = M_PI / stacks;
 
-	// trianglulos do topo
-	for(int i = 0 ; i < slices ; i++){
-		float rstack = sin(stackStep); // raio da primeira stack
-		points.push_back(Point(0.0,radius,0.0)); // ponto superior
-		points.push_back(Point(rstack*cos((i+1)*sliceStep), radius*cos(stackStep), rstack*sin((i+1)*sliceStep)));
-		points.push_back(Point(rstack*cos(i*sliceStep), radius*cos(stackStep), rstack*sin(i*sliceStep)));
-	}
-
-	// triangulos do meio
-	for(int i = 1 ; i < (stacks-1) ; i++ ){
-		float rstack = sin(i*stackStep); // raio da stack atual
-		float rstack_next = sin((i+1)*stackStep); // raio da stack seguinte
+	for(int i = 0 ; i < stacks ; i++ ){
+		float rstack = radius*sin(i*stackStep); // raio da stack atual
+		float rstack_next = radius*sin((i+1)*stackStep); // raio da stack seguinte
 		for(int j = 0 ; j < slices ; j++){
 			points.push_back(Point(rstack*cos(j*sliceStep), radius*cos(i*stackStep), rstack*sin(j*sliceStep)));
 			points.push_back(Point(rstack_next*cos((j+1)*sliceStep), radius*cos((i+1)*stackStep), rstack_next*sin((j+1)*sliceStep)));
@@ -32,14 +23,6 @@ int generateSphere(float radius, int slices, int stacks, string file_name){
 			points.push_back(Point(rstack_next*cos((j+1)*sliceStep), radius*cos((i+1)*stackStep), rstack_next*sin((j+1)*sliceStep)));
 			points.push_back(Point(rstack*cos(j*sliceStep), radius*cos(i*stackStep), rstack*sin(j*sliceStep)));
 		}
-	}
-
-	// triangulos do fundo
-	for(int i = 0 ; i < slices ; i++){
-		float rstack = sin(stackStep); // raio da ultima stack
-		points.push_back(Point(0.0,-radius,0.0)); // ponto inferior
-		points.push_back(Point(rstack*cos(i*sliceStep), radius*cos((stacks - 1)*stackStep), rstack*sin(i*sliceStep)));
-		points.push_back(Point(rstack*cos((i+1)*sliceStep), radius*cos((stacks - 1)*stackStep), rstack*sin((i+1)*sliceStep)));
 	}
 
 	// necessário operações sobre ficheiros
