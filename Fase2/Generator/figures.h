5b7065b0ab3dcc333d334b0abd78b7c05f57cfbf
joaop21 <jpsilva9898@gmail.com>
2019-03-24 22:11:56 +0000

    documentacao

--- Fase2/Generator/figures.h
+++ Fase2/Generator/figures.h
@@ -90,13 +90,48 @@ namespace figures{
     *
     *  @returns Integer que dita se a geração foi um sucesso ou não
     */
-
     int generateCone(float bottom_radius, float height, int slices, int stacks, std::string file_name);
 
+    /**
+    *  @brief Função que gera o pontos para a realização dos devidos triângulos, e
+    * conseguentemente gravação em ficheiro.
+    *
+    *  @param inner_radius - parâmetro que é o raio de cada anel do torus.
+    *  @param inner_radius - parâmetro que é a distância desde o centro do torus até à figura.
+    *  @param nsides - parâmetro que é o número de lados de cada anel da figura.
+    *  @param nrings - parâmetro que é o números de anéis da figura.
+    *  @param file_name - parâmetro que é o nome do ficheiro onde se devem gravar os
+    * pontos e triangulos gerados.
+    *
+    *  @returns Integer que dita se a geração foi um sucesso ou não
+    */
     int generateTorus(float innerRadius, float outerRadius, int nsides, int nrings, std::string file_name);
 
+    /**
+    *  @brief Função que gera um ficheiro XML que contém todas as coordenadas dos
+    * asteróides a que foram gerados
+    *
+    *  @param num_asteroids - parâmetro que é o número de asteróides a serem gerados.
+    *  @param min_radius - parâmetro que é a distância mínima dos asteróides ao sol.
+    *  @param mas_radius - parâmetro que é a distância máxima dos asteróides ao sol.
+    *  @param file_name - parâmetro que é o nome do ficheiro onde se deve gravar tudo
+    * o que foi gerado anteriormente.
+    *
+    *  @returns Integer que dita se a geração foi um sucesso ou não
+    */
     int generateAsteroids(int num_asteroids, int min_radius, int max_radius, std::string file_name);
 
+
+    /**
+    *  @brief Função guarda em ficheiro todos os pontos passados como parâmetro.
+    *
+    *  @param points - Parâmetro que é um vector que contém todos os pontos a serem
+    * gravados em ficheiro.
+    *  @param file_name - parâmetro que é o nome do ficheiro onde se devem gravar os
+    * pontos e triangulos gerados.
+    *
+    *  @returns Integer que dita se a geração foi um sucesso ou não
+    */
     int write_in_file(std::vector<Point> points, std::string file_name);
 
 }
