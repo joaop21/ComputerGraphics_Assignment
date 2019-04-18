6752573961bbcf8c2b5226ec090e7b2b3a613bf8
joaop21 <jpsilva9898@gmail.com>
2019-04-18 17:52:30 +0100

    catmull passado para o geometry

--- Fase3/Engine/main.cpp
+++ Fase3/Engine/main.cpp
@@ -16,100 +16,7 @@ using namespace tinyxml2;
 using namespace std;
 using namespace geometry;
 
-#define POINT_COUNT 8
-
 Tree tree_struct;
-float p[POINT_COUNT][3];
-
-void vector_to_matrix(vector<Point> points){
-	vector<Point>::iterator it;
-	int i = 0;
-	for(it = points.begin() ; it != points.end() ; it++,i++){
-		p[i][0] = it->x;
-		p[i][1] = it->y;
-		p[i][2] = it->z;
-	}
-}
-
-void multMatrixVector(float *m, float *v, float *res) {
-
-	for (int j = 0; j < 4; ++j) {
-		res[j] = 0;
-		for (int k = 0; k < 4; ++k) {
-			res[j] += v[k] * m[j * 4 + k];
-		}
-	}
-
-}
-
-void getCatmullRomPoint(float t, float *p0, float *p1, float *p2, float *p3, float *pos, float *deriv) {
-
-	float m[4][4] = {	{-0.5f,  1.5f, -1.5f,  0.5f},
-						{ 1.0f, -2.5f,  2.0f, -0.5f},
-						{-0.5f,  0.0f,  0.5f,  0.0f},
-						{ 0.0f,  1.0f,  0.0f,  0.0f}};
-
-	float ax[4], ay[4], az[4];
-	float px[4], py[4], pz[4];
-	px[0] = p0[0]; px[1] = p1[0]; px[2] = p2[0]; px[3] = p3[0];
-	py[0] = p0[1]; py[1] = p1[1]; py[2] = p2[1]; py[3] = p3[1];
-	pz[0] = p0[2]; pz[1] = p1[2]; pz[2] = p2[2]; pz[3] = p3[2];
-
-	multMatrixVector(*m, px, ax);
-	multMatrixVector(*m, py, ay);
-	multMatrixVector(*m, pz, az);
-
-	float tpos[4] = { t*t*t, t*t, t, 1};
-	pos[0] = 0; pos[1] = 0; pos[2] = 0;
-	for(int i=0; i<4; i++){
-		pos[0] += tpos[i] * ax[i];
-		pos[1] += tpos[i] * ay[i];
-		pos[2] += tpos[i] * az[i];
-	}
-
-	float tderiv[4] = { 3*t*t, 2*t, 1, 0};
-	deriv[0] = 0; deriv[1] = 0; deriv[2] = 0;
-	for(int i=0; i<4; i++){
-		deriv[0] += tderiv[i] * ax[i];
-		deriv[1] += tderiv[i] * ay[i];
-		deriv[2] += tderiv[i] * az[i];
-	}
-}
-
-void getGlobalCatmullRomPoint(float gt, float *pos, float *deriv) {
-
-	float t = gt * POINT_COUNT; // this is the real global t
-	int index = floor(t);  // which segment
-	t = t - index; // where within  the segment
-
-	int indices[4];
-	indices[0] = (index + POINT_COUNT-1)%POINT_COUNT;
-	indices[1] = (indices[0]+1)%POINT_COUNT;
-	indices[2] = (indices[1]+1)%POINT_COUNT;
-	indices[3] = (indices[2]+1)%POINT_COUNT;
-
-	getCatmullRomPoint(t, p[indices[0]], p[indices[1]], p[indices[2]], p[indices[3]], pos, deriv);
-}
-
-void renderCatmullRomCurve() {
-
-// desenhar a curva usando segmentos de reta - GL_LINE_LOOP
-
-	float pos[3], deriv[3];
-	float tam = 1000; // nº de divisoes que vou querer fazer
-	float deltaT = 1.0/tam; // vai ajudar a estabelecer a "divisao em que estou"
-	glColor3f(0.86f, 0.86f, 0.86f); // grey
-	glBegin(GL_LINE_LOOP);
-
-		// percorro todas as divisoes, e o globalt multiplico pela divisao que estabeleci, desde 0 até 1
-		for(int i=0; i<tam; i++){
-			getGlobalCatmullRomPoint(deltaT*i,pos,deriv);
-			glVertex3f(pos[0],pos[1],pos[2]);
-		}
-
-	glEnd();
-}
-
 
 void draw_figure(Figure fig) {
 
@@ -146,23 +53,10 @@ void draw_tree(Tree t) {
 	glPushMatrix();
 
 	if (!t.head_figure.translation.empty){
-
-		vector_to_matrix(t.head_figure.translation.points);
-		renderCatmullRomCurve();
-
-		float pos[3], deriv[3];
-		float time_p = glutGet(GLUT_ELAPSED_TIME)/(t.head_figure.translation.time*1000);
-
-		getGlobalCatmullRomPoint(time_p,pos,deriv);
-		glTranslatef(pos[0],pos[1],pos[2]);
+		t.head_figure.translation.apply_translation();
 	}
 	if (!t.head_figure.rotation.empty){
-		float angle ;
-		if(t.head_figure.rotation.time == -1)
-			angle = t.head_figure.rotation.angle;
-		else angle = glutGet(GLUT_ELAPSED_TIME)*360/(t.head_figure.rotation.time*1000);
-
-		glRotatef(angle, t.head_figure.rotation.x, t.head_figure.rotation.y, t.head_figure.rotation.z);
+		t.head_figure.rotation.apply_rotation();
 	}
 	if (!t.head_figure.scale.empty)
 		glScalef(t.head_figure.scale.x, t.head_figure.scale.y, t.head_figure.scale.z);
@@ -322,7 +216,7 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(1000,800);
+	glutInitWindowSize(1200,800);
 	glutCreateWindow("Engine");
 
 // struct load
